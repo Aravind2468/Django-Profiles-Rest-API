25909a614c9b715c424abbd02924ea1d3cfba17e
Aravind <aravindreddyramidi9@gmail.com>
2021-08-07 16:25:09 +0530

    Added viewset to profiles api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -30,6 +30,20 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
         
         return user
+class MODELNAME(models.Model):
+    """Model definition for MODELNAME."""
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for MODELNAME."""
+
+        verbose_name = 'MODELNAME'
+        verbose_name_plural = 'MODELNAMEs'
+
+    def __str__(self):
+        """Unicode representation of MODELNAME."""
+        pass
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
