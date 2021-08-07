bf4ccdb75d2edc17d8d9ba3d31f1a067e98fa3bf
Aravind <aravindreddyramidi9@gmail.com>
2021-08-07 13:53:56 +0530

    Added HelloApiview

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """ Test API View """ 
+
+    def get(self, request, format=None):
+        """ Returns a list of APIView features """
+        an_apiview = [
+            'My Name is Aravind',
+            'I am currently working in TCS',
+            'I have good knowledge on Python'
+        ]
+
+        return Response({'message':'Hello!', 'apiview':an_apiview})
